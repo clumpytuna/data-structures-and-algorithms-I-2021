1e3c61ce959aa74ad49221f9608890823ce2ce91
morell <d-9797@mail.ru>
2021-02-27 10:07:52 +0300

    homework: stack comments

--- homework/stack/stack.cpp
+++ homework/stack/stack.cpp
@@ -1,66 +1,50 @@
 
 namespace task {
 
-class List {
+class Stack {
 public:
 
-    List();
-    List(size_t count, const int& value = int());
-    List& operator=(const List& other);
-    ~List();
-
-    int& Front() {
-        // Your code goes here...
+    Stack() {
+        // Initialize an empty stack
     }
-    const int& Front() const {
-        // Your code goes here...
+    Stack(const Stack& other) {
+        // Copies data from "other" stack into "this" stack (on initialization)
     }
-    int& Back();
-    const int& Back() const;
+    Stack& operator=( const Stack& other ) {
+        // Copy data from "other" stack into "this" stack (on assignment)
+    }
+    ~Stack() {
+        // Releases resources
+    }
 
-    bool Empty() const;
-    size_t Size() const;
-    
-    void Clear();
-    void PushBack(const int& value);
-    void PopBack();
-    void PushFront(const int& value);
-    void PopFront();
-    void Resize(size_t count);
+    int Top() {
+        // Returns last added element
+    }
+
+    bool Empty() const {
+        // Returns whether the stack has no elements
+    }
+    size_t Size() const {
+        // Returns the number of elements
+    }
+
+    void Push(int value) {
+        // Inserts an element at the top
+    }
+    void Pop() {
+        // Removes last added element
+    }
+    void Clear() {
+        // Removes all elements from the stack
+    }
 
 private:
 
-    struct Node {
+    class Node {
         // Your code goes here...
     };
 
     // Your code goes here...
-};
-
-class Stack {
-public:
-
-    Stack();
-    Stack(const Stack& other);
-    Stack& operator=( const Stack& other );
-    ~Stack();
-
-    int& Top() {
-        // Your code goes here...
-    }
-    const int& Top() const {
-        // Your code goes here...
-    }
-
-    bool Empty() const;
-    size_t Size() const;
-
-    void Push(const int& value);
-    void Pop();
-    void Clear();
-
-private:
-    List container;
 };
 
 } 
