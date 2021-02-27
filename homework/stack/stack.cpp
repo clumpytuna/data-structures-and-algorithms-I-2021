7e84e96024a15ef2e6ee36ce9b0809e6288401c7
morell <d-9797@mail.ru>
2021-02-27 11:11:06 +0300

    homework: stack cpplint

--- homework/stack/stack.cpp
+++ homework/stack/stack.cpp
@@ -1,18 +1,17 @@
-
-namespace task {
-
 class Stack {
-public:
-
+ public:
     Stack() {
         // Initialize an empty stack
     }
+
     Stack(const Stack& other) {
         // Copies data from "other" stack into "this" stack (on initialization)
     }
-    Stack& operator=( const Stack& other ) {
+
+    Stack& operator=(const Stack& other) {
         // Copy data from "other" stack into "this" stack (on assignment)
     }
+
     ~Stack() {
         // Releases resources
     }
@@ -24,6 +23,7 @@ public:
     bool Empty() const {
         // Returns whether the stack has no elements
     }
+
     size_t Size() const {
         // Returns the number of elements
     }
@@ -31,23 +31,22 @@ public:
     void Push(int value) {
         // Inserts an element at the top
     }
+
     void Pop() {
         // Removes last added element
     }
+
     void Clear() {
         // Removes all elements from the stack
     }
 
-private:
-
+ private:
     class Node {
         // Your code goes here...
     };
 
     // Your code goes here...
 };
-
-} 
 
 int main() {
     return 0;
